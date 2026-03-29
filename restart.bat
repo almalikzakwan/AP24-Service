fb5e382c6268aa9e9aa65c8a14ca713c03ef17b6
almalikzakwan <almalikzakwan@gmail.com>
2026-03-29 22:35:13 +0800

    make service name as varibale in restart.bat

--- restart.bat
+++ restart.bat
@@ -1,6 +1,14 @@
 @echo off
+
+REM set your service_name file path here
+set "filepath=E:\Apache\httpd-2.4.66-260223-Win64-VS18\AP24-Service\service_name.txt"
+
+for /f "usebackq delims=" %%i in (%filepath%) do (
+    set "service_name=%%i"
+)
+
 echo "Restarting bitch Apache2D service....."
-net stop Apache2D
-net start Apache2D
+net stop %service_name%
+net start %service_name%
 echo "Apache2D successfully fucking restart"
 pause
