a158bdbce2946a853109079c88f8a4bb6269abd7
almalikzakwan <almalikzakwan@gmail.com>
2026-04-16 02:13:25 +0800

    set fixed path for service_name.txt in <stop>|<start>|<restart>|<status> .bat

--- stop.bat
+++ stop.bat
@@ -1,7 +1,10 @@
 @echo off
 
-REM set your service_name file path here
-set "filepath=E:\Apache\httpd-2.4.66-260223-Win64-VS18\AP24-Service\service_name.txt"
+REM cd into current directory
+cd /d %~dp0
+
+REM automatic get your current path here
+set "filepath=%CD%\service_name.txt"
 
 for /f "usebackq delims=" %%i in (%filepath%) do (
     set "service_name=%%i"
