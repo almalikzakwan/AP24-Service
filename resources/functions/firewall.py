5f920174b30e43f02f490686fba1b798914dd1c2
almalikzakwan <almalikzakwan@gmail.com>
2026-05-12 15:01:25 +0800

    add retun value

--- resources/functions/firewall.py
+++ resources/functions/firewall.py
@@ -1,7 +1,7 @@
 import subprocess
 
 class firewall:
-    def addInbound(port, action="allow", protocol="TCP", direction="in"):
+    def addInbound(port, action="allow", protocol="TCP", direction="in") -> bool:
         """
         Adds an inbound firewall rule to the Windows Firewall.
         If not add, windows will not allow custom port to be access.
@@ -21,10 +21,13 @@ class firewall:
 
         try:
             subprocess.run(command, check=True, capture_output=True, text=True)
+            return True
         except subprocess.CalledProcessError as e:
             print(f"Failed to add rule Apache Custom Port {port}")
+            return False
         except Exception as e:
             print(f"An unexpected error occured: {e}")
+            return False
 
     def portForwarding(port, listenport):
         """
@@ -45,5 +48,7 @@ class firewall:
 
         try:
             subprocess.run(command, check=True, capture_output=True, text=True)
+            return True
         except Exception as e:
             print(f"An unexcepted error occured: {e}")
+            return False
