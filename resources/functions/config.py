8626e1ebaf19d1ca925ec7d998c77e233f71bb34
almalikzakwan <almalikzakwan@gmail.com>
2026-05-12 11:35:36 +0800

    set default parameter default path

--- resources/functions/config.py
+++ resources/functions/config.py
@@ -2,20 +2,18 @@ from functions.root import root as r
 from functions.file import files as f
 
 class config:
-    def path():
+    def path(dp: str = "../../config/development.conf") -> tuple:
         """
         file that need to change port
         change with your folder instead (my example was conf/extra/developments folder) 
         """
-        dp = f"../../config/development.conf"
         files = f(dp)
-        developments = files.read(dp)
+        developments = files.read()
 
-        r = r()
         path = r.path()
         return [
             f"{path}/conf/httpd.conf",
             f"{path}/conf/extra/httpd-ssl.conf",
             f"{path}/conf/extra/httpd-vhosts.conf", 
-            f"{developments}"
+            f"{path}/{developments}"
         ]
