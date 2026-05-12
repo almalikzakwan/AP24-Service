20ad76f5675cfbbfdf323cace59740f674dff466
almalikzakwan <almalikzakwan@gmail.com>
2026-05-12 15:00:43 +0800

    make 2 function for root class. Apache24 and AP24 service path

--- resources/functions/root.py
+++ resources/functions/root.py
@@ -1,6 +1,11 @@
 import os
 
 class root:
-    def path():
-        cwd = os.getcwd()
-        return f"{cwd.replace("\\","/")}/../../../Apache24"
+    def __init__(self):
+        self.cwd = os.getcwd()
+
+    def ApachePath(self):
+        return f"{self.cwd.replace("\\","/")}/../../Apache24"
+    
+    def AP24Path(self):
+        return f"{self.cwd.replace("\\","/")}/../"
