b2ea9a556f74ea5e626a27b4a84c8e16819bfd2f
almalikzakwan <almalikzakwan@gmail.com>
2026-05-12 11:37:32 +0800

    firewall class for addInbound and port forwarding

---
+++ resources/functions/firewall.py
@@ -0,0 +1,49 @@
+import subprocess
+
+class firewall:
+    def addInbound(port, action="allow", protocol="TCP", direction="in"):
+        """
+        Adds an inbound firewall rule to the Windows Firewall.
+        If not add, windows will not allow custom port to be access.
+        """
+        command = [
+            "netsh",
+            "advfirewall",
+            "firewall",
+            "add",
+            "rule",
+            f"name=Apache Custom Port {port}",
+            f"dir={direction}",
+            f"action={action}",
+            f"protocol={protocol}",
+            f"localport={port}"
+        ]
+
+        try:
+            subprocess.run(command, check=True, capture_output=True, text=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to add rule Apache Custom Port {port}")
+        except Exception as e:
+            print(f"An unexpected error occured: {e}")
+
+    def portForwarding(port, listenport):
+        """
+        port forwarding for default and ssl port
+        forwarding 80 and 443 into custom port.
+        """
+        command = [
+            "netsh",
+            "interface",
+            "portproxy",
+            "add",
+            "v4tov4",
+            f"listenport={listenport}",
+            "listenaddress=0.0.0.0",
+            f"connectport={port}",
+            "connectaddress=127.0.0.1"
+        ]
+
+        try:
+            subprocess.run(command, check=True, capture_output=True, text=True)
+        except Exception as e:
+            print(f"An unexcepted error occured: {e}")
