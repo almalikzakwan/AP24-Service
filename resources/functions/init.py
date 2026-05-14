2b41c7f044a10371ca0b9da06fe48c92de78cd7e
almalikzakwan <almalikzakwan@gmail.com>
2026-05-14 08:12:35 +0800

    try except if something wrong happen during execution.

--- resources/functions/init.py
+++ resources/functions/init.py
@@ -11,48 +11,53 @@ class kickoff:
     def init():
         pts = ["default","ssl"]
         for pt in pts:
-            # make file path string
-            rt = root()
-            fp = f"{rt.AP24Path()}/storage/recent.{pt}.port"
+            try:
+                # make file path string
+                rt = root()
+                fp = f"{rt.AP24Path()}/storage/recent.{pt}.port"
 
-            # init file class
-            file = f(fp)
+                # init file class
+                file = f(fp)
 
-            # Validate file existence. else will get recent configured port.
-            if not os.path.isfile(fp):
-                print(f"[ERROR] File '{fp}' does not exists. Creating '{fp}' with default port...")
-                op = "80" if pt == "default" else "443"
-                file.write(op)
-                print(f"[INFO] '{fp}' file successfully create.")
-            else:
-                op = file.read()
-
-            
-            # get a random number
-            np = randoms.randint(4430, 65536)
-            # save random port into file
-            file.write(str(np))
-            
-            # file or directory that need to change custom port
-            # this is my directory that need to change port in Apache24
-            # my example for developments was conf/extra/developments, change your path instead in config/development.conf.
-            confs = config.path()
-
-            for cf in confs:
-                # check if path is file or directory
-                if not os.path.isfile(cf):
-                    for fn in os.listdir(cf):
-                        cfp = os.path.join(cf, fn)
-                        if os.path.isdir(cfp):
-                            continue
-                        r.string(cfp, op, np)
+                # Validate file existence. else will get recent configured port.
+                if not os.path.isfile(fp):
+                    print(f"[ERROR] File '{fp}' does not exists. Creating '{fp}' with default port...")
+                    op = "80" if pt == "default" else "443"
+                    file.write(op)
+                    print(f"[INFO] '{fp}' file successfully create.")
                 else:
-                    r.string(cf, op, np)
+                    op = file.read()
 
-            # add firewall rile to allow custom port to be access
-            firewall.addInbound(np)
-            # port forwarding default and ssl into new custom
-            firewall.portForwarding(np, 80 if pt == "default" else 443)
+                
+                # get a random number
+                np = randoms.randint(4430, 65536)
+                # save random port into file
+                file.write(str(np))
+                
+                # file or directory that need to change custom port
+                # this is my directory that need to change port in Apache24
+                # my example for developments was conf/extra/developments, change your path instead in config/development.conf.
+                confs = config.path()
+
+                for cf in confs:
+                    # check if path is file or directory
+                    if not os.path.isfile(cf):
+                        for fn in os.listdir(cf):
+                            cfp = os.path.join(cf, fn)
+                            if os.path.isdir(cfp):
+                                continue
+                            r.string(cfp, op, np)
+                    else:
+                        r.string(cf, op, np)
+
+                # add firewall rile to allow custom port to be access
+                firewall.addInbound(np)
+                # port forwarding default and ssl into new custom
+                firewall.portForwarding(np, 80 if pt == "default" else 443)
+            except:
+                if 'file' in locals and 'op' in locals:
+                    file.write(op)
+                print("An unexpected error oocur.")
 
         #configure php
         php.write()
