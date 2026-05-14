c626f89a18d12865a95d6192fb0fef7b7b43cc37
almalikzakwan <almalikzakwan@gmail.com>
2026-05-14 08:18:48 +0800

    try except.

--- resources/functions/php.py
+++ resources/functions/php.py
@@ -4,21 +4,25 @@ from functions.file import files as f
 class php:
     """ Create php configuration in Apache24 folder """
     def write() -> bool:
-        root = r()
-        path = f"{root.ApachePath()}/conf/php.conf"
-        APachefile = f(path)
+        try: 
+            root = r()
+            path = f"{root.ApachePath()}/conf/php.conf"
+            APachefile = f(path)
 
-        fp = f"{root.AP24Path()}/config/php.conf"
-        file = f(fp)
-        php = file.read()
+            fp = f"{root.AP24Path()}/config/php.conf"
+            file = f(fp)
+            php = file.read()
 
-        string = f'''
-LoadFile "{php}/php8ts.dll"
-LoadModule php_module "{php}/php8apache2_4.dll"
-AddType application/x-httpd-php .php
-PHPIniDir "{php}/"
-'''
-        APachefile.write(string)
+            string = f'''
+    LoadFile "{php}/php8ts.dll"
+    LoadModule php_module "{php}/php8apache2_4.dll"
+    AddType application/x-httpd-php .php
+    PHPIniDir "{php}/"
+    '''
+            APachefile.write(string)
 
-        return True
+            return True
+        
+        except:
+            print('An error occured, something wrong happen.')
 
