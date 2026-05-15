1097fb592bf7e92586cdbcf743c539ef53d2f4ec
almalikzakwan <almalikzakwan@gmail.com>
2026-05-15 17:44:59 +0800

    init function and add port function

--- resources/functions/config.py
+++ resources/functions/config.py
@@ -2,20 +2,27 @@ from functions.root import root as r
 from functions.file import files as f
 
 class config:
-    def path(dp: str = "config/development.conf") -> tuple:
+    """ return path of config files """
+    def __init__(self):
+        self.rt = r()
+        self.AP24Path = self.rt.AP24Path()
+
+    def developments(self, dp: str = "config/development.conf") -> tuple:
         """
         file that need to change port
         change with your folder instead (my example was conf/extra/developments folder) 
         """
-        rt = r()
-        AP24Path = rt.AP24Path()
-        files = f(f"{AP24Path}/{dp}")
+        files = f(f"{self.AP24Path}/{dp}")
         developments = files.read()
 
-        APPath = rt.ApachePath()
+        APPath = self.rt.ApachePath()
         return [
             f"{APPath}/conf/httpd.conf",
             f"{APPath}/conf/extra/httpd-ssl.conf",
             f"{APPath}/conf/extra/httpd-vhosts.conf", 
             f"{APPath}/{developments}"
         ]
+    
+    def ports(self, dp:str = "config/ports.conf"):
+        """ return config/ports.conf """
+        return f"{self.AP24Path}/{dp}"
