7a3d67aed3934b92e64b14d665c246ddc1818eac
almalikzakwan <almalikzakwan@gmail.com>
2026-05-18 10:07:33 +0800

    ongoing: todo

--- resources/functions/init2.py
+++ resources/functions/init2.py
@@ -7,34 +7,43 @@ from functions.random import randoms
 from functions.firewall import firewall
 from functions.replace import replace as r
 from functions.root import root
-import ast
+import traceback
 
 class kickoff:
     def init():
-        conf = config()
-        confs = conf.developments()
-        cfpp = conf.path('config/ports.conf')
-        portsf = f(cfpp)
-        ports = portsf.readlines()
-        conf_ports = {}
-        for i, port in enumerate(ports):
-            filename = re.sub(r"\{.*?\}|\s+","",port)
-            cpath = [item for item in confs if filename in item][0]
-            value = re.search(r'\{(.*?)\}', port)
-            for item in value.group(1).strip().split(","):
-                key, type = item.strip().split(":")
-                #todo: port, type, path , read and write into developement config
-                #todo: multiple port forwarding, firewall inbound rule.
-                nprt = randoms.randint(4430, 65535)
-                ports[i] = ports[i].replace(str(key),str(nprt))
-                reps = portsf.writelines(ports)
-                if reps is not True:
-                    print("[Warning] New port cannot been changed.")
-                else:
-                    rfp = conf.path(f"storage/recent.{type}.port")
-                    rfile = f(rfp)
-                    rfile.write(f"\n{str(nprt)}")
+        try:
+            conf = config()
+            confs = conf.developments()
+            cfpp = conf.path('/config/ports.conf')
+            portsf = f(cfpp)
+            rports = portsf.readlines()
+
+            for i, path in enumerate(confs):
+                fn = os.path.basename(path)
+                ports = [item for item in rports if fn in item][0].strip()
+                port = re.search(r"\{(.*?)}",ports)
+                for item in port.group(1).strip().split(","):
+                    oprt, type = item.strip().split(":")
+                    nprt = randoms.randint(4430, 65535)
+                    #todo: port, type, path , read and write into developement config
+                    #todo: multiple port forwarding, firewall inbound rule.
+                    rports[i] = rports[i].replace(str(oprt),str(nprt))
+                    reps = portsf.writelines(rports)
+                    if reps is not True:
+                        print("[Warning] New port cannot been change in config/ports.conf")
+                    else:
+                        rfp = conf.path(f"/storage/recent.{type}.port")
+                        rfile = f(rfp)
+                        rfile.write(f"{str(nprt)}\n")
+
+                    r.string(path, str(oprt), str(nprt))
                     
+
+                    
+                            
+
+        except Exception as e:
+            print(f"[ERROR] {traceback.print_exc()}")
                  
 
                     
