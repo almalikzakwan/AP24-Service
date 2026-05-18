d2cff88d7bbb8688191fd798da25c54fdd32e0eb
almalikzakwan <almalikzakwan@gmail.com>
2026-05-18 10:06:33 +0800

    add and append into path list and ports config

--- resources/functions/config.py
+++ resources/functions/config.py
@@ -1,3 +1,5 @@
+import os
+import re
 from functions.root import root as r
 from functions.file import files as f
 
@@ -5,24 +7,41 @@ class config:
     """ return path of config files """
     def __init__(self):
         self.rt = r()
+        self.ApachePath = self.rt.ApachePath()
         self.AP24Path = self.rt.AP24Path()
     
-    def developments(self, dp: str = "config/development.conf") -> tuple:
+    def developments(self, dp: str = "/config/development.conf") -> tuple:
         """
         file that need to change port
         change with your folder instead (my example was conf/extra/developments folder) 
         """
-        files = f(f"{self.AP24Path}/{dp}")
-        developments = files.read()
-
-        APPath = self.rt.ApachePath()
-        return [
+        
+        APPath = self.ApachePath
+        path = [
             f"{APPath}/conf/httpd.conf",
             f"{APPath}/conf/extra/httpd-ssl.conf",
-            f"{APPath}/conf/extra/httpd-vhosts.conf", 
-            f"{APPath}/{developments}"
+            f"{APPath}/conf/extra/httpd-vhosts.conf",
         ]
+        files = f(self.path(dp))
+        developments = files.read()
+        rank = 2
+        for fn in os.listdir(f"{APPath}/{developments}"):
+            rank += 1
+            cfp = os.path.join(f"{APPath}/{developments}", fn).replace("\\","/")
+            path.append(cfp)
+            pathlength = len(path)
+            confports = self.path("/config/ports.conf")
+            cfiles = f(confports)
+            lines = cfiles.readlines()
+            if pathlength > len(lines):
+                string = re.sub(r"httpd-vhosts.conf", fn, lines[2])
+                reps = cfiles.write(f"{string}")
+                if reps is not True:
+                    print(f"Config name cannot be added into ports.conf -> Config: {fn}")
+        
+        return path
     
+    #fix: must be duplicated code. clean needed
     def path(self, dp:str):
         """ return path from given path """
-        return f"{self.AP24Path}{dp}"
+        return f"{self.AP24Path}/{dp}"
